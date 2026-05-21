64ee76b6074d6a5db7942a544a64a0dd1a548d8b
Rafael Afonso <122814044+rafae1-afonso@users.noreply.github.com>
2026-05-21 16:49:13 -0300

    Automatização do valor das propriedades do objeto de produto

--- src/main/java/xbrain_challenge/rest_api/database/object/ProductObject.java
+++ src/main/java/xbrain_challenge/rest_api/database/object/ProductObject.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 @Getter
 @Setter
@@ -19,14 +20,16 @@ public class ProductObject {
 
     @Column(nullable = false)
     @Pattern(regexp = "^PRODUCT-\\d{3}$", message = "The product code must match the format: PRODUCT-000.")
-    private String productCode = "PRODUCT-XXX";
+    private String productCode = "PRODUCT-" + (new Random().nextInt(999-100+1) + 100);
 
-    @Column(nullable = false)
+    @Column(nullable = false, scale = 2)
     @Positive(message = "The product price must be positive.")
     @NotNull(message = "The product price can't be NULL.")
-    private BigDecimal price = new BigDecimal(0);
+    private BigDecimal price = new BigDecimal(
+            (new Random().nextInt(100) / 100.0) + (new Random().nextInt(1000) + 1)
+    );
 
     @Column(nullable = false)
     @Positive(message = "The product quantity must be positive.")
-    private Integer quantity = 1;
+    private Integer quantity = new Random().nextInt(5) + 1;
 }
